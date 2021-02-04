799155f648313357a294970362c701bbafb1775a
jeje.e <jeje.e@kakaocorp.com>
2021-02-04 10:54:56 +0900

    compose

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,16 @@
 
 import io.reactivex.Observable
+import io.reactivex.ObservableTransformer
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    
+    //compose
+    Observable.range(1,10).compose(handleCompose()).subscribe{
+        println(it)
+    }
+}
+
+fun <T> handleCompose() : ObservableTransformer<T,T> {
+    return ObservableTransformer { it.doOnNext{println(it)} }
 }
